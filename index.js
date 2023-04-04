45ce7e756f08519f39ad4a4964ef0d3f0e966d2d
email <bethkimaku@gmail.com>
2023-04-04 10:27:02 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const namesArray=["Guadalupe","Otis","Aki"]
+function writeCards(namesArray){
+    const cards=[]
+    for(let i=0; i<namesArray.length; i++){
+        const message= `Thank you, ${namesArray[i]}, for the wonderful surprise gift!`
+        cards.push(message)
+    }
+  return cards
+}
+writeCards(names)
+
+function countDown(num){
+    let i=num 
+    while (i>=0){
+        console.log (i)
+        i--
+    }
+}
+countDown(10)
